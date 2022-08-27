c1c15a8e92ffb1a706a0a42f7a626d477f77a7a1
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-08-27 11:42:05 -0400

    Initial commit

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -6,11 +6,46 @@
 //
 
 import SwiftUI
+enum Buttons {
+	case one
+	case two
+	case three
+	case four
+	case five
+	case six
+	case seven
+	case eight
+	case nine
+	case zero
+	case plus
+	case minus
+	case multiply
+	case divide
+	case negative
+	case equal
+	case root
+	case clear
+	case decimal
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		ZStack{
+			Color.white.edgesIgnoringSafeArea(.all)
+			
+			VStack {
+				HStack {
+					Text("0")
+						.bold()
+						.font(.system(size: 64))
+						.foregroundColor(.black)
+					Spacer()
+				}
+				.padding()
+				
+				ForEach(
+			}
+		}
     }
 }
 
